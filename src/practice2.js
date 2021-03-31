e3c0f347b922bdf60edf18cb2b3d6261ee130063
Aaron Young <hi@aaronyoung.dev>
2021-03-31 06:35:53 -0400

    fix problem

--- src/practice2.js
+++ src/practice2.js
@@ -3,9 +3,9 @@ const axios = require("axios");
 const urlToFetch = "https://api.pro.coinbase.com/products";
 /**
  * create a function, getCurrencies that uses the above url
- * fetch and obtain the display_name, base_currency, status
- * return one object per currency
- * documentation for provided url is here
+ * fetch and obtain the display_name, then sort by the display name and return
+ * the first 5
+ * the documentation for provided url is here
  * https://docs.pro.coinbase.com/#get-products
  */
 
